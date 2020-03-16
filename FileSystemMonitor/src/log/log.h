7d86f3fcd179b11bd874281a0131fff26f090d79
lidorco <lidorco@outlook.com>
2020-03-16 15:33:31 +0200

    Add log

---
+++ FileSystemMonitor/src/log/log.h
@@ -0,0 +1,28 @@
+
+#define LOG(...) LogWrapper(__FILE__, __LINE__, __VA_ARGS__)
+#include <sstream>
+#include <Windows.h>
+
+template<typename... Args>
+void LogWrapper(const char* file, int line, const Args&... args)
+{
+	std::ostringstream msg;
+	LogRecursive(file, line, msg, args...);
+}
+
+template<typename T, typename... Args>
+void LogRecursive(const char* file, int line, std::ostringstream& msg,
+	T value, const Args&... args)
+{
+	msg << value;
+	LogRecursive(file, line, msg, args...);
+}
+
+void LogRecursive(const char* file, int line, std::ostringstream& msg)
+{
+	std::ostringstream logLine;
+	logLine << file << "(" << line << "): " << msg.str() << std::endl;
+	OutputDebugStringA(logLine.str().c_str());
+	printf(logLine.str().c_str());
+
+}
---
+++ FileSystemMonitor/src/main.cpp
@@ -0,0 +1,10 @@
+#include "log\log.h"
+
+
+int main() {
+	
+	LOG("File System Monitor started");
+	
+	return 0;
+}
+
